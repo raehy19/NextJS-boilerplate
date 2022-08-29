a72f7da1e5c1ca49578665b8063ff44291ca8b0e
Raehyeon Jeong <raehy19@gmail.com>
2022-08-30 02:14:56 +0900

    Feat : set globalStyle, theme

---
+++ styles/globalStyle.ts
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+  ${reset};
+  
+  * {
+    box-sizing: border-box;
+  }
+  
+  html {
+    body {
+      font-size: 16px;
+    }
+  }
+`;
+
+export default GlobalStyle;
